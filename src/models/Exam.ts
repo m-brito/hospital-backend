14f78e4f4ed8521f66cf870b8586993e6c2abf56
m-brito <britomauricio2003@gmail.com>
2024-09-17 22:56:26 -0300

    feat: create Exam model

---
+++ src/models/Exam.ts
@@ -0,0 +1,24 @@
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, JoinColumn, BaseEntity } from 'typeorm';
+import { Appointment } from './Appointment';
+
+@Entity()
+export class Exam extends BaseEntity {
+    @PrimaryGeneratedColumn()
+    id!: number;
+
+    @Column()
+    type!: string;
+
+    @Column()
+    date!: Date;
+
+    @Column()
+    result!: string;
+
+    @Column()
+    status!: string;
+
+    @ManyToOne(() => Appointment)
+    @JoinColumn()
+    appointment!: Appointment;
+}
