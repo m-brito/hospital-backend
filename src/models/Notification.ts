0e20aff4b725eb68d377f64872d3abad3e60eb63
m-brito <britomauricio2003@gmail.com>
2024-09-17 22:57:05 -0300

    feat: create Notification model

---
+++ src/models/Notification.ts
@@ -0,0 +1,21 @@
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, JoinColumn, BaseEntity } from 'typeorm';
+import { User } from './User';
+
+@Entity()
+export class Notification extends BaseEntity {
+    @PrimaryGeneratedColumn()
+    id!: number;
+
+    @Column()
+    type!: string;
+
+    @Column()
+    datetime!: Date;
+
+    @Column('text')
+    message!: string;
+
+    @ManyToOne(() => User)
+    @JoinColumn()
+    user!: User;
+}
