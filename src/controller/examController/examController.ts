3fc7436c778441f278085c318ba0838bb13e2c4f
m-brito <britomauricio2003@gmail.com>
2024-09-21 20:37:51 -0300

    feat: creates route to find exams

--- src/controller/examController/examController.ts
+++ src/controller/examController/examController.ts
@@ -12,3 +12,31 @@ export const createExam = async (req: Request, res: Response) => {
         return res.status(500).json({ message: 'Internal Server Error' });
     }
 }
+
+export const updateExam = async (req: Request, res: Response) => {
+    const examId = Number(req.params.id);
+
+    try {
+        const updatedExam = await examService.updateExam(examId, req.body);
+        return res.status(200).json(updatedExam);
+    } catch (error) {
+        if (error instanceof Error) {
+            return res.status(400).json({ message: error.message });
+        }
+        return res.status(500).json({ message: 'Internal Server Error' });
+    }
+};
+
+export const getExams = async (req: Request, res: Response) => {
+    try {
+        const user = req.user;
+        if(!user) return res.status(400).json({ message: 'User not found' });
+        const exams = await examService.getExams(user);
+        return res.status(200).json(exams);
+    } catch (error) {
+        if (error instanceof Error) {
+            return res.status(400).json({ message: error.message });
+        }
+        return res.status(500).json({ message: 'Internal Server Error' });
+    }
+};
